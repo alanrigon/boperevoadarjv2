fc75799235a0f2253e131700be67ea755212d95d
alanrigon <alanrigon845@gmail.com>
2025-07-03 00:22:20 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
   apiKey: "AIzaSyC1NmXqw17a59ZAVAyKRJ_EYzEHJyMb0Ro",
   authDomain: "sequestros-bope-revoada-rj.firebaseapp.com",
   projectId: "sequestros-bope-revoada-rj",
   storageBucket: "sequestros-bope-revoada-rj.firebasestorage.app",
   messagingSenderId: "713711634210",
-  appId: "1:713711634210:web:033d43b9d63817cd07077b
+  appId: "1:713711634210:web:033d43b9d63817cd07077b"
 };
